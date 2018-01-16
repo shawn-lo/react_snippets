fec59ffb175d69ac3f1dbbbc53858180099c7881
Shawn Luo <shawn.luotianyu@gmail.com>
2018-01-16 14:49:02 -0500

    delect duplicates

--- src/actions/action_record.js
+++
@@ -1,9 +0,0 @@
-export function selectRecord(record) {
-    console.log(record.id);
-    return {
-        type: 'RECORD_SELECTED',
-        id: record.id,
-        text: record.content,
-        payload: record
-    };
-}
--- src/actions/action_transfer.js
+++
@@ -1,6 +0,0 @@
-export function onclick (btn_id) {
-    return {
-        type: 'TRANS_BTN_CLICKED',
-        id: btn_id,
-    };
-}
